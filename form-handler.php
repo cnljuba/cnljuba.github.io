f823286d8c15b25a7d368e11735a81720315468e
Casey Ljuba <54912640+cnljuba@users.noreply.github.com>
2021-05-03 16:18:24 -0400

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -1,13 +1,26 @@
 <?php
-$name = $_POST[''];
-$visitor_email = $_POST[''];
-$subject = $_POST[''];
-$message = $_POST[''];
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
 
+$email_from = 'info@cnljuba.github.io';
 
+$email_subject = 'New Form Submission';
 
+$email_body = "User name: $name.\n".
+              "User Email: $visitor_email.\n".
+              "Subject: $subject.\n".
+              "User Message: $message.\n";
 
+$to = 'casey.ljuba@gmail.com';
 
+$headers = "From: $email_from \r\n";
 
+$headers .= "Reply-To: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: index.html");
 
 ?>
